610b07bc4476bcffbddb3590d52e4b8735227d1f
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-07-02 09:09:03 -0300

    Aiming joystick working

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -9,11 +9,23 @@ public class TestScript : MonoBehaviour
     private void Start()
     {
         inputReader.OnMoveEvent += InputReader_Move;
+        inputReader.OnShootEvent += InputReader_Shoot;
+        inputReader.OnAimEvent += InputReader_Aim;
+    }
+
+    private void InputReader_Aim(Vector2 aim)
+    {
+        Debug.Log($"Aim event: {aim}");
+    }
+
+    private void InputReader_Shoot(bool isShoot)
+    {
+        Debug.Log($"Shot event: {isShoot}");
     }
 
     private void InputReader_Move(Vector2 movement)
     {
-        Debug.Log($"Move event triggered with vector: {movement}");
+        Debug.Log($"Move event: {movement}");
     }
 
     private void OnDestroy()
